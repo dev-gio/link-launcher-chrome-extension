7712599b29e9f75b3e39c8037888b4e958980b9f
dev-gio <ghio.cabang@gmail.com>
2019-11-11 16:33:01 +0800

    Updates on events

--- popup.js
+++ popup.js
@@ -1,11 +1,33 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+function linkEvent() {
     let links = document.querySelectorAll('.link-container');
 
     Array.from(links).forEach(link => {
         link.addEventListener('click', function(event) {
-            let href = link.querySelector('a').getAttribute("href");
+            let href = link.querySelector('span').getAttribute("data-url");
             console.log(href);
             window.open(href, '_blank');
         });
     });
+}
+
+function loadLinks() {
+    fetch('sites.json')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = '';
+            data.forEach(function (link) {
+                output += `
+                    <div class="link-container">
+                        <span data-url="${link.url}">${link.title}</span>
+                    </div>
+                `;
+            });
+
+            document.querySelector('.popup-body').innerHTML = output;
+            linkEvent();
+        });
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    loadLinks();
 });
